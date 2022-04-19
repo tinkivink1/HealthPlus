93843bbfd27a0e23989b0a4b86f66ef71c5d49cd
Роман Муковнин <RoMukovnin@mail.ru>
2022-04-19 19:59:51 +0300

    Добавил файл LearnMore, на который теперь сслыются кнопки с мастер-страницы.

--- HealthPlus/Controllers/HomeController.cs
+++ HealthPlus/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace HealthPlus.Controllers
             return View();
         }
 
+        public IActionResult LearnMore()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
