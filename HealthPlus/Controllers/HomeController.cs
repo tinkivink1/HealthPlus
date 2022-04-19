79b63e6dba6d19d537b1ebbfce2e2d84343deb65
Роман Муковнин <RoMukovnin@mail.ru>
2022-04-19 23:31:26 +0300

    Добавлен файл Login. Изменена кнопка LearnMore (временно)

--- HealthPlus/Controllers/HomeController.cs
+++ HealthPlus/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace HealthPlus.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
