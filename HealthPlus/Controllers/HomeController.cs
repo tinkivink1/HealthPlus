e2a15c788c0d6ea358736ae7fc5780b9781c60fd
Роман Муковнин <RoMukovnin@mail.ru>
2022-04-22 00:07:12 +0300

    Изменил текст. Добавил файл с кнопкой для регестрации. Фото не работают, не понял почему

--- HealthPlus/Controllers/HomeController.cs
+++ HealthPlus/Controllers/HomeController.cs
@@ -43,6 +43,11 @@ namespace HealthPlus.Controllers
             return View();
         }
 
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
