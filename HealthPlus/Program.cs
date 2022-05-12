7c44705f4f9f122043ed1069705c07cda241a3b2
Роман Муковнин <RoMukovnin@mail.ru>
2022-05-13 01:34:39 +0300

    ОТкатил использование Entity

--- HealthPlus/Program.cs
+++ HealthPlus/Program.cs
@@ -1,16 +1,13 @@
 using HealthPlus.Models;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
-
-builder.Services.AddDbContext<ApplicationContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationContext")));
 
 var app = builder.Build();
 
@@ -21,6 +18,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 
 app.UseHttpsRedirection();
